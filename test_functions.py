6e0e06b0df7525576533ae92351254351faa0470
Miguelro <36134758+Miguelro@users.noreply.github.com>
2021-10-04 20:32:39 +0200

    Testing code examples

--- test_functions.py
+++ test_functions.py
@@ -128,3 +128,71 @@ def is_tribonacci(num):
                  return True
             else:
                 break
+
+
+#####################
+#Decorators
+#######################
+def perform_twice(fn, *args, **kwargs):
+    fn(*args, **kwargs)
+    fn(*args, **kwargs)
+
+
+perform_twice(print, 5, 10, sep='&', end='...')
+print(5, 10, sep='&', end='...')
+
+def make_divisibility_test(n):
+    def divisible_by_n(m):
+        return m % n == 0
+    return divisible_by_n
+
+div_by_3 = make_divisibility_test(3)
+div_by_3 # => True
+list(filter(div_by_3,range(4)))
+tuple(filter(div_by_3, range(10)))  # => (0, 3, 6, 9)
+make_divisibility_test(5)(10)  # => True
+
+
+def print_args(function):
+    def wrapper(*args, **kwargs):
+        print(args, kwargs)
+        return function(*args, **kwargs)
+    return wrapper
+
+def compute(x, y, z=1):
+    return (x + y) * z
+
+compute(3, 5, z=2)
+# => 16
+
+compute_log = print_args(compute)
+compute_log(3, 5, z=2)
+
+
+
+compute = print_args(compute)
+compute(3, 5, z=2)
+compute.__name__
+
+@print_args
+def compute(x, y, z=1):
+    return (x + y) * z
+compute(3, 5, z=2)
+compute.__name__
+
+import functools
+#functools.wraps(function) decorator updates the wrapper function 
+# so that it clones important properties from function.
+def print_args(function):
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        print(args, kwargs)
+        return function(*args, **kwargs)
+    return wrapper
+
+@print_args
+def compute(x, y, z=1):
+    return (x + y) * z
+
+compute(3, 5, z=2)
+compute.__name__
